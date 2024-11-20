bbbf5a71c0c5187fd2369492e01cb7d240be6c80
Adniloet Sosa <adni.sori@gmail.com>
2024-11-20 13:44:28 +0100

    JS terminado

--- js/script.js
+++ js/script.js
@@ -6,16 +6,12 @@
 // imprime el chiste en un nuevo LI de la UL
 // sumamos este chiste al ARRAY [] de chiste
 
-let arrChiste = []
-
-let chistes = localStorage.getItem("chistesGuardados")
+let arrChiste = JSON.parse(localStorage.getItem("chistesGuardados")) || [];
 
 const btn = document.getElementById("fetchJoke")
 const list = document.getElementById("jokeList")
+const btnEliminar = document.getElementById('botonEliminar');
 
-console.log(chistes);
-
-list.innerText = chistes
 
 btn.addEventListener("click", () => {
     fetch("https://api.chucknorris.io/jokes/random")
@@ -32,15 +28,55 @@ btn.addEventListener("click", () => {
         })
 
         .then((data) => {
-        console.log(data);
+        //console.log(data);
         
-        list.insertAdjacentHTML("beforeend",
-            `
-            <li>${data.value}</li>
-            <button class="btnEliminar">Eliminar</button>
-            `
-            ) 
-  
+            
+            list.insertAdjacentHTML("beforeend",
+                `
+                <li>${data.value}</li>
+                <button class="btnEliminar">Eliminar</button>
+                `
+                )       
+            
+            arrChiste.push(data.value);
+            keepJokes();
+            btnEliminar.classList.remove('oculto');
+
         })
         
     })
+
+    const keepJokes = () => {
+        localStorage.setItem("chistesGuardados", JSON.stringify(arrChiste));
+    }
+
+    const getJokes = () => {
+        arrChiste.forEach(chiste => {
+            list.insertAdjacentHTML("beforeend",
+                `
+                <li>${chiste}</li>
+                <button class="btnEliminar">Eliminar</button>
+                `
+                )  
+        });
+        
+    }
+
+    btnEliminar.addEventListener('click', () => {
+        list.innerHTML = '';
+        localStorage.clear();
+        btnEliminar.classList.add('oculto');
+    })
+
+    if(arrChiste.length === 0) {
+        btnEliminar.classList.add('oculto')
+    } else {
+        btnEliminar.classList.remove('oculto');
+    }
+    getJokes();
+
+// - Manejador de click en el botón "Obtener Chiste"
+// - Una función para obtener un chiste de Chuck Norris desde la API --> jokesStorage
+// - Una función para renderizar la lista de chistes en el DOM
+// - Una función para guardar la lista de chistes en localStorage -->keepJokes
+// - Una función para cargar la lista de chistes desde localStorage -->getJokes
